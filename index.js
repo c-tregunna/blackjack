6b4a502f30893bd147165cd1662bbbe10a15f0b3
Claire Tregunna <c.tregunna1980@gmail.com>
2023-01-17 17:56:28 +0000

    added functionaltiy to start new game on win or loss

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
 let playerEl = document.getElementById("player-el");
+let startBtn = document.getElementById("start-btn");
+let cardBtn = document.getElementById("card-btn");
 
 playerEl.textContent = player.name + ": $" + player.chips;
 
@@ -35,9 +37,11 @@ function startGame() {
     isAlive = true;
     let firstCard = getRandomCard();
     let secondCard = getRandomCard();
-    cards = [firstCard, secondCard]; 
+    cards = [firstCard, secondCard];
     sum = firstCard + secondCard;
     renderGame();
+
+    startBtn.style.display = "none";
 }
 
 //Show the game on page and the actual game play, win lose etc
@@ -53,8 +57,10 @@ function renderGame() {
     } else if ( sum === 21) {
         message = "You've got Blackjack!"
         hasBlackJack = true;
+        startBtn.style.display = "block";
     } else {
         message = "You're out of the game!"
+        startBtn.style.display = "block";
         isAlive = false;
     }
     //Renders message on page if you can draw crad, got blackjack or lost
@@ -64,7 +70,7 @@ function renderGame() {
 //Select a new card to add to the sum
 function newCard() {
     // Only allow the player to get a new card if she IS alive and does NOT have Blackjack
-    if (isAlive === true && hasBlackJack === false) {
+    if (isAlive === true && hasBlackJack === false || isAlive === true && hasBlackJack === true) {
         let card = getRandomCard();
         sum += card; // Add the new card to the sum variable
         cards.push(card); // adds the new card to the array
